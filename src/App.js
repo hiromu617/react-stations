420f2bfe7264ab018fe3148bdb4dcc3a3e60e3c6
hiro <hiromhiro617@gmail.com>
2021-08-14 14:01:05 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // DO NOT DELETE
-
 import * as React from 'react'
 import { useState } from 'react'
 import './App.css'
@@ -8,30 +7,37 @@ import './App.css'
  *
  * @type {React.FC}
  */
+const INITIAL_URL = 'https://images.dog.ceo/breeds/dachshund/dachshund-6.jpg'
+const END_POINT = 'https://dog.ceo/api/breeds/image/random'
+
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState(
-    'https://images.dog.ceo/breeds/dachshund/dachshund-6.jpg',
-  )
-  const END_POINT = 'https://dog.ceo/api/breeds/image/random'
-  const setRandomUrl = () => {
-    fetch(END_POINT)
+  const [dogUrl, setDogUrl] = useState(INITIAL_URL)
+  const [loading, setLoading] = useState(false)
+  const setRandomUrl = async () => {
+    setLoading(true)
+    await fetch(END_POINT)
       .then(res => res.json())
       .then(result => {
         if (result.status === 'success') setDogUrl(result.message)
         else alert('An error occured')
       })
+    setLoading(false)
   }
 
   return (
-    <div style={{ padding: '10px' }}>
+    <div>
       <header>
         <h1>Dog Application</h1>
       </header>
-      <p>犬の画像を表示するサイトです</p>
-      <img width="400px" src={dogUrl} />
-      <button onClick={() => setRandomUrl()} style={{ display: 'block' }}>
-        更新
-      </button>
+      <div className="container">
+        <p>犬の画像を表示するサイトです</p>
+        <img height="400px" src={dogUrl} />
+      </div>
+      <div className="btn-wrap">
+        <button onClick={() => setRandomUrl()} className="btn" disabled={loading}>
+          {loading ? "更新中...": "更新"}
+        </button>
+      </div>
     </div>
   )
 }
