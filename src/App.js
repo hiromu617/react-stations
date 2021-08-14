73a11b28f055effb47424172f7f2dae3b2299898
hiro <hiromhiro617@gmail.com>
2021-08-14 13:33:37 +0900

    station5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
-import {useState} from 'react'
+import { useState　} from 'react'
 import './App.css'
 
 /**
@@ -9,18 +9,31 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/dachshund/dachshund-6.jpg")
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/dachshund/dachshund-6.jpg',
+  )
+  const [loading, setLoading] = useState(false)
+
+  const setRandomUrl = () => {
+    setLoading(true)
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(result => {
+        if (result.status === 'success') setDogUrl(result.message)
+        else alert("An error occured")
+      })
+      .finally(() => setLoading(false))
+  }
+
   return (
-    <div style={{padding: "10px"}}>
+    <div style={{ padding: '10px' }}>
       <header>
         <h1>Dog Application</h1>
       </header>
       <p>犬の画像を表示するサイトです</p>
-      <img
-        width="400px"
-        src={dogUrl}
-      />
-      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/spaniel-sussex/n02102480_4640.jpg')}>更新</button>
+      {loading && <p>更新中</p>}
+      <img width="400px" src={dogUrl} />
+      <button onClick={() => setRandomUrl()} style={{display: "block"}}>更新</button>
     </div>
   )
 }
